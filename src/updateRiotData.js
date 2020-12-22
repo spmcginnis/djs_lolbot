2a134f5a3ef7e6fee33d5edd00cee55d45a6531b
spm <spmcginnis@users.noreply.github.com>
2020-12-22 16:56:56 -0600

    API call now checks for ETag code

--- src/updateRiotData.js
+++ src/updateRiotData.js
@@ -12,15 +12,31 @@ function getChampList() {
 // Single file with limited info on each champion:
 // http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/champion.json
     const QSTRING = ROOT + PATCH + BRANCH + FILE_NAME
-    console.log(QSTRING);
-    AXIOS.get(QSTRING)
-        .then(function (response) {
-            writeData(response.data, "./src/data/champion.json")
-        })
-        .catch(function (error) {
-            console.log("error: " + error)
-        })
+    
+    let config = {
+        headers: {
+            // TODO save this ETag value locally and update with this script
+            "If-None-Match": '"de23f9a427b1d49dbfd102e8496908ed"'
+        }
+    }
+    // In order to continue chaining, we have to return the async function that we are waiting on
+    return (
 
+        AXIOS.get(QSTRING,config)
+            .then(
+                function (response) {
+                writeData(response.data, "./src/data/champion.json")  
+            })
+            .catch(function (error) {
+                if (error.response.status === 304) {
+                    console.log("Local file up to date")
+                } else {
+                    console.log("error: " + error)
+                    throw error   
+                }
+
+            })
+    )
 }
 
 // TODO Writes the data to local json files
@@ -32,6 +48,7 @@ function writeData(data, filePath) {
 
     FS.writeFileSync(filePath, data);
 
+    console.log(`Write complete: ${filePath}`)
 
 }
 
@@ -41,4 +58,25 @@ function writeData(data, filePath) {
 
 // TODO lolbot update command from discord
 
-getChampList();
+// getChampList().then(() => {
+//     const CHAMP_LIST = JSON.parse(FS.readFileSync("./src/data/champion.json", "utf-8"))
+
+//     for ( let champ in CHAMP_LIST.data) {
+//         console.log(CHAMP_LIST.data[champ].name)
+//     }
+
+// })
+async function main () {
+    await getChampList()    
+
+    const CHAMP_LIST = JSON.parse(FS.readFileSync("./src/data/champion.json", "utf-8"))
+
+    for ( let champ in CHAMP_LIST.data) {
+        console.log(CHAMP_LIST.data[champ].name)
+}
+}
+
+main()
+
+
+

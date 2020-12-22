3736871821cc96b980c87b9341599bc8684eb8e3
spm <spmcginnis@users.noreply.github.com>
2020-12-22 17:36:08 -0600

    now uses the etag value to check the api for updates and also stores the new etag value if there has been a change

--- src/updateRiotData.js
+++ src/updateRiotData.js
@@ -6,6 +6,11 @@ const ROOT = "http://ddragon.leagueoflegends.com/cdn/";
 const PATCH = "10.25.1/";
 const BRANCH =  "data/en_US/";
 
+// Etag for version checking
+let etag = FS.readFileSync("./src/data/etag.txt", "utf-8");
+let hasNewEtag = false;
+
+console.log("First etag: ", etag)
 // Queries the Riot API
 function getChampList() {
     const FILE_NAME = "champion.json";
@@ -13,67 +18,65 @@ function getChampList() {
 // http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/champion.json
     const QSTRING = ROOT + PATCH + BRANCH + FILE_NAME
     
+    // sets the request header to check the etag
     let config = {
         headers: {
             // TODO save this ETag value locally and update with this script
-            "If-None-Match": '"de23f9a427b1d49dbfd102e8496908ed"'
+            "If-None-Match": etag
         }
     }
     // In order to continue chaining, we have to return the async function that we are waiting on
     return (
-
         AXIOS.get(QSTRING,config)
-            .then(
-                function (response) {
-                writeData(response.data, "./src/data/champion.json")  
-            })
-            .catch(function (error) {
-                if (error.response.status === 304) {
-                    console.log("Local file up to date")
-                } else {
-                    console.log("error: " + error)
-                    throw error   
-                }
-
-            })
+        .then(
+            // this only fires if the etag of the resource is different than the one stored locally
+            function (response) {
+            writeData(response.data, "./src/data/champion.json")
+            // stores the updated etag 
+            etag = response.headers.etag
+            hasNewEtag = true;
+        })
+        .catch(function (error) {
+            if (error.response.status === 304) {
+                console.log("Local file up to date. Etag: " + etag)
+            } else {
+                console.log("error: " + error)
+                throw error   
+            }
+        })
     )
 }
 
 // TODO Writes the data to local json files
 function writeData(data, filePath) {
     data = JSON.stringify(data);
-    
-    // testing
-    console.log(filePath);
-
     FS.writeFileSync(filePath, data);
-
     console.log(`Write complete: ${filePath}`)
-
 }
 
-// TODO Keeps a log of the most recent updates
-
-// TODO Creates an index of champion names, aliases, and ids.
-
-// TODO lolbot update command from discord
-
-// getChampList().then(() => {
-//     const CHAMP_LIST = JSON.parse(FS.readFileSync("./src/data/champion.json", "utf-8"))
-
-//     for ( let champ in CHAMP_LIST.data) {
-//         console.log(CHAMP_LIST.data[champ].name)
-//     }
-
-// })
-async function main () {
-    await getChampList()    
-
+function updateMetadata() {
     const CHAMP_LIST = JSON.parse(FS.readFileSync("./src/data/champion.json", "utf-8"))
 
+    // TODO save index of champ names, aliases, and ids
     for ( let champ in CHAMP_LIST.data) {
         console.log(CHAMP_LIST.data[champ].name)
+    }
 }
+
+async function setEtag(etag) {
+    hasNewEtag = false;
+    return FS.writeFileSync("./src/data/etag.txt", etag)
+}
+
+async function main () {
+    // use await to wait for the completion of a function-- that function must return a promise.
+    await getChampList()
+    if (hasNewEtag) {
+        await setEtag(etag)
+        console.log("new etag set")
+    }
+    
+    // updateMetadata()
 }
 
 main()
